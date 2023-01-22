026c4d362c76868b0a911ed64881a6eee4c45672
Alondrob <alondrob@gmail.com>
2023-01-21 22:38:48 -0500

    depl

--- server/index.js
+++ server/index.js
@@ -5,14 +5,31 @@ const PropertyModel = require("./models/Property");
 const bodyParser = require("body-parser");
 const propertyRouter = require("./routes/propertiesRoutes");
 const userRouter = require("./routes/userRoutes");
+const path = require('path');
 
 const app = express();
 app.use(cors());
+const _dirname = path.dirname("")
+const buildPath = path.join(_dirname, "../client/build")
+app.use(express.static(buildPath));
+
+app.get("/", (req, res) => {
+  res.sendFile(
+     path.join(_dirname, "../client/build/index.html"),
+    (err) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  )
+})
+
+
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const mongoDBUrl = process.env.MONGODB_URL;
-// const mongoDBUrl = "mongodb+srv://adrob1983:Lam1910@book-vacation-cluster.vqjxuoh.mongodb.net/bookVacDB?retryWrites=true&w=majority";
 
 mongoose.connect(mongoDBUrl, {
   useNewUrlParser: true,
